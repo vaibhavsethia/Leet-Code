195ba4aadb577c52b56df948d975898c65f2bcef
vaibhavsethia <vaibhavsethia1998@gmail.com>
2020-08-11 01:00:28 +0530

    First Problem

---
+++ Easy/RunningSumOf1DArray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> Sol;
+        long long int Sum = 0;
+        
+        for(int i = 0; i<nums.size(); i++){
+            Sum += nums[i];
+            Sol.push_back(Sum);
+        }
+        
+        return Sol;
+    }
+};
